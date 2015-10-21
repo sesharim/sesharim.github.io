458b7559037f138da3fab72920fe26bb05b71a6a
Lukin Maksim <lukin.maksim@gmail.com>
2015-10-21 09:53:35 +0300

    updates for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,48 +1,36 @@
-# config valid only for current version of Capistrano
 lock '3.4.0'
+server '107.170.122.37', user: 'root', roles: %w{app db web}
 
-set :application, 'customgears'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'CustomGears'
+set :repo_url, 'git@github.com:sesharim/customgears.git'
+set :branch, :master
+set :deploy_to, '/home/rails'
+set :scm, :git
+set :user, :root
+set :use_sudo, false
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :format, :pretty
+set :keep_releases, 3
+set :log_level, :info
+set :pty, true
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :ssh_options, {
+ forward_agent: true
+}
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+before 'deploy:update', 'deploy:update_jekyll'
 
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  [:start, :stop, :restart, :finalize_update].each do |t|
+    desc "#{t} task is a no-op with jekyll"
+    task t, :roles => :app do ; end
   end
 
+  desc 'Run jekyll to update site before uploading'
+  task :update_jekyll do
+    # clear existing _site
+    # build site using jekyll
+    # remove Capistrano stuff from build
+    %x(rm -rf _site/* && jekyll build && rm _site/Capfile && rm -rf _site/config)
+  end
 end
